b69210928ede68378320790ee5151518302842a9
sytem <sytem.out@hotmail.com>
2019-07-29 17:40:22 +0800

    解决css问题
    修改打包的属性

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Board from '@/components/Board'
-import ws from '@/components/Ws'
 
 Vue.use(Router)
 
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'Board',
       component: Board
-    },
-    {
-      path: '/ws',
-      name: 'ws',
-      component: ws
     }
   ]
 })
